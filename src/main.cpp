953c5f79af0a6a14021d647a2fe9de4edcc41071
SilvaUnCompte <silvablocks@gmail.com>
2023-11-14 16:40:45 +0100

    LEDManager

--- src/main.cpp
+++ src/main.cpp
@@ -10,11 +10,33 @@ void setup()
   Serial.println(TinyGPSPlus::libraryVersion());
 }
 
+void LEDManager(int hour, int minute)
+{
+  int hour_pin_tab[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
+  int minute_pin_tab[] = {0, 1, 2, 3, 4, 5};
+  minute = floor(minute / 10);
+
+  for (int i = 0; i < 13; i++)
+  {
+    digitalWrite(hour_pin_tab[i], hour_pin_tab[i] == hour);
+  }
+
+  for (int i = 0; i < 6; i++)
+  {
+    digitalWrite(minute_pin_tab[i], minute_pin_tab[i] == minute);
+  }
+}
+
 void loop()
 {
-  if (Serial2.available() > 0)
-  {
-    GPSTime t = getGPSTime();
-    Serial.print(t.hour + ":" + t.minute);
-  }
+  GPSTime time = getGPSTime();
+
+  Serial.print(F("Time: "));
+  Serial.print(time.hour);
+  Serial.print(F(":"));
+  Serial.println(time.minute);
+
+  delay(1000);
+
+  LEDManager(time.hour, time.minute);
 }
