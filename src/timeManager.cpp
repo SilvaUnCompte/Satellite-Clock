8ba16021de853f2f266d7c2a37351854e03cd54f
SilvaUnCompte <silvablocks@gmail.com>
2024-05-05 16:23:59 +0200

    Clean time manager

--- src/timeManager.cpp
+++ src/timeManager.cpp
@@ -1,8 +1,3 @@
-// #include <WiFi.h>
-// #include <time.h>
-// #include "wifiManager.h"
-// #include "LEDManager.h"
-
 #include <Arduino.h>
 #include <WiFi.h>
 #include "time.h"
@@ -27,72 +22,22 @@ TimeObj getCurrentTime()
   return {timeinfo.tm_hour, timeinfo.tm_min};
 }
 
-// void updateLocalTime()
-// {  
-//   if (WiFi.status() != WL_CONNECTED)
-//   {
-//     setAllLED(true);
-//     return;
-//   }
-
-//   // init and get the time
-//   configTime(gmtOffset_sec, daylightOffset_sec, ntpServer);
-//   Serial.println("Time updated.");
-//   getCurrentTime();
-// }
-
-
-
-// void printLocalTime(){
-//   struct tm timeinfo;
-//   if(!getLocalTime(&timeinfo)){
-//     Serial.println("Failed to obtain time");
-//     return;
-//   }
-//   Serial.println(&timeinfo, "%A, %B %d %Y %H:%M:%S");
-//   Serial.print("Day of week: ");
-//   Serial.println(&timeinfo, "%A");
-//   Serial.print("Month: ");
-//   Serial.println(&timeinfo, "%B");
-//   Serial.print("Day of Month: ");
-//   Serial.println(&timeinfo, "%d");
-//   Serial.print("Year: ");
-//   Serial.println(&timeinfo, "%Y");
-//   Serial.print("Hour: ");
-//   Serial.println(&timeinfo, "%H");
-//   Serial.print("Hour (12 hour format): ");
-//   Serial.println(&timeinfo, "%I");
-//   Serial.print("Minute: ");
-//   Serial.println(&timeinfo, "%M");
-//   Serial.print("Second: ");
-//   Serial.println(&timeinfo, "%S");
-
-//   Serial.println("Time variables");
-//   char timeHour[3];
-//   strftime(timeHour,3, "%H", &timeinfo);
-//   Serial.println(timeHour);
-//   char timeWeekDay[10];
-//   strftime(timeWeekDay,10, "%A", &timeinfo);
-//   Serial.println(timeWeekDay);
-//   Serial.println();
-// }
-
 void updateLocalTime(){
 
   // Connect to Wi-Fi
   Serial.print("Connecting to ");
   Serial.println(ssid);
   WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) {
+  while (WiFi.status() != WL_CONNECTED) { // TODO: Faire sécu si arrive pas à se co du tout !!!!
     delay(500);
     Serial.print(".");
   }
-  Serial.println("");
-  Serial.println("WiFi connected.");
+  Serial.println(" WiFi connected.");
   
   // Init and get the time
   configTime(gmtOffset_sec, daylightOffset_sec, ntpServer);
-  TimeObj test = getCurrentTime();
+  Serial.println("Time updated.");
+  getCurrentTime();
 
   //disconnect WiFi as it's no longer needed
   WiFi.disconnect(true);
